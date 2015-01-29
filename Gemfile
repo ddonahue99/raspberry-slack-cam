28dd2ac8e527566b789fd172a5a758fe7d0e247f
Dave Donahue <ddonahue@instructure.com>
2015-01-29 21:05:00 +0000

    set up s3 daemon

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 gem 'awesome_print'
 gem 'sinatra'
 gem 'thin'
+gem 'aws-sdk'
+gem 'nokogiri', '1.6.5'
 
 group :development, :test do
   gem 'rake'
---
+++ daemon.rb
@@ -0,0 +1,36 @@
+require 'aws-sdk'
+
+class PhotoDaemon
+  attr_accessor :config_file
+
+  def initialize(config_file)
+    @config_file = config_file
+  end
+
+  def s3_config
+    @_s3_config ||= YAML.load_file(config_file)['s3']
+  end
+ 
+  def s3
+    @_s3 ||= AWS::S3.new(s3_config)
+  end
+  
+  def upload_file
+    file_name = File.join("public/photo.jpg")
+    key = File.basename(file_name)
+    s3.buckets[s3_config['bucket_name']].objects[key].write(:file => file_name)
+  end
+
+  def run!
+    while(true) do
+      puts "Taking picture"
+      `raspistill -o public/photo.jpg -w 800 -h 600`
+      puts "Uploading to S3"
+      upload_file
+      puts "Taking a nap.."
+      sleep 5
+    end
+  end
+end
+
+PhotoDaemon.new(File.join("config.yml")).run!
